265face26a316b524b6eb3d7032e394a6bdc02fa
davidSoutarson <triplaxs@gmail.com>
2019-01-04 20:46:32 +0100

    enfin  fini pyramide.rb

--- thp_ruby/pyramide.rb
+++ thp_ruby/pyramide.rb
@@ -1,10 +1,11 @@
 puts "Bonjour bien venu dans ma super piramide! combien etage veux tu ?"
 print "entrez un nombre entre 1 et 25 >"
-n = gets.chomp.to_i 
+stage = gets.chomp.to_i 
 
-n=25
-while n >= 1
-  puts "* " * n
-  n = n -1 
+n = 1
+while stage >= n
+  puts (" " * (stage - n)) + ("#" * n)
+  n += 1
 end
+
 
